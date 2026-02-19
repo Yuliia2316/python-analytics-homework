5e7e940a9baf6c54d5c226a03cab15248622a5cf
Yuliia <kobyliatska2316@gmail.com>
2026-02-19 19:41:38 +0200

    add average comment

--- analysis.py
+++ analysis.py
@@ -3,4 +3,5 @@ data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 950, 500]}
 df = pd.DataFrame(data)
 print("Продажі по містах:")
 print(df)
+# Розрахунок середнього значення продажів
 print("Середнє значення:", df["sales"].mean())
